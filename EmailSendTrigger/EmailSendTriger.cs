811ad317a8b530329717ad6a1bb3ba9a20471eae
Serhii <peskosergij@gmail.com>
2023-03-09 19:39:34 -0500

    Removed sendgriapikey from code

--- EmailSendTrigger/EmailSendTriger.cs
+++ EmailSendTrigger/EmailSendTriger.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Web;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -17,11 +19,11 @@ namespace EmailSendTrigger
         [FunctionName("EmailSendTriger")]
         public async Task Run([BlobTrigger("files/{name}")]Stream myBlob, string name, ILogger log)
         {
-            var sendGridApiKey = "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";
-            var client = new SendGridClient(sendGridApiKey);
-            string emailStorageConnection = "DefaultEndpointsProtocol=https;AccountName=internshipstorage2002000;AccountKey=T1iuBTdtjnOk1CnWTHQS578P+AJmI8M5Ya2jg9X/V+Nme3XSq/zZBfBh3tFJsLUC+o+mHKRUiuO4+AStaeKsbg==;EndpointSuffix=core.windows.net";
+            
+            string emailStorageConnection = Environment.GetEnvironmentVariable("EmailStorageConnection");
             string emailContainerName = "emails";
             string email = "";
+            string apikey = Environment.GetEnvironmentVariable("SendGridApiKey");
             var blobServiceClient = new BlobServiceClient(emailStorageConnection);
             var blobContainerClient = blobServiceClient.GetBlobContainerClient(emailContainerName);
             var blobClient = blobContainerClient.GetBlobClient(name + ".txt");
@@ -35,7 +37,8 @@ namespace EmailSendTrigger
                 }
             }
             var uri = Uri.EscapeUriString($"https://internshipstorage2002000.blob.core.windows.net/files/{name}");
-            var msg = new SendGridMessage()
+            var sendGridClient = new SendGridClient(apikey);
+            var message = new SendGridMessage()
             {
                 From = new EmailAddress("speshko@uwaterloo.ca", "Serhii Peshko"),
                 Subject = "File uploading",
@@ -43,9 +46,8 @@ namespace EmailSendTrigger
                 HtmlContent = $"<h2>Congratulations!</h2><p>Your file <a href=\"{uri}\">{name}</a> was uploaded successfully</p>"
             };
 
-            msg.AddTo(new EmailAddress(email));
-            var response = await client.SendEmailAsync(msg);
-
+            message.AddTo(new EmailAddress(email));
+            var response = await sendGridClient.SendEmailAsync(message);
             log.LogInformation(response.IsSuccessStatusCode ? "Email queued successfully!" : "Something went wrong");
         }
     }
